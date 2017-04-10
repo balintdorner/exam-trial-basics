549afdf2d15e461615258317c10ae230279f8c9e
Balint Dorner <dorner.balint@gmail.com>
2017-04-10 14:31:03 +0200

    exam-trial-basics

--- seconds/seconds.py
+++ seconds/seconds.py
@@ -1,5 +1,12 @@
-def seconds(change_this):
-    pass
+def seconds(num_list):
+    number_list = num_list
+    new_list = []
+
+    for number in number_list:
+        if number % 2 == 0:
+            new_list.append(number)
+
+    return new_list
     # Create a function that takes a list as a parameter,
     # and returns a new list with every second element from the orignal list
     # example: [1, 2, 3, 4, 5] should produce [2, 4]
