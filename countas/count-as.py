dc53a85365ea8d7f3294c14d78c42e68301b4a9b
Balint Dorner <dorner.balint@gmail.com>
2017-04-10 14:53:08 +0200

    exercise count-as: FileNotFoundError appears, the function does not work properly, solution is wanted

--- countas/count-as.py
+++ countas/count-as.py
@@ -1,8 +1,22 @@
-def count_as(change_this):
-    pass
-    # Create a function that takes a filename as string parameter,
-    # counts the occurances of the letter "a", and returns it as a number.
-    # If the file does not exist, the function should return 0 and not break.
+# Create a function that takes a filename as string parameter,
+# counts the occurances of the letter "a", and returns it as a number.
+# If the file does not exist, the function should return 0 and not break.
+
+my_file = open('afile.txt', 'r')
+
+def count_as(file_name):
+    try:
+        my_file = open(file_name, 'r')
+        text = my_file.read()
+        occcurances = text.count('a')
+        return text
+    except FileNotFoundError:
+        return 0
+
+
+
+
+
 
 print(count_as("afile.txt")) # should print 28
 print(count_as("not-a-file")) # should print 0
